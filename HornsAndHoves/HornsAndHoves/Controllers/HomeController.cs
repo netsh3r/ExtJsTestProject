14b5bc6c1145f61937f79c72665949d7b09c73db
Eugene Kostyukov <evgenii.kostiukov@gmail.com>
2022-02-03 17:20:37 +0300

    Реализация таблицы со списком организаций

--- HornsAndHoves/HornsAndHoves/Controllers/HomeController.cs
+++ HornsAndHoves/HornsAndHoves/Controllers/HomeController.cs
@@ -1,4 +1,10 @@
-﻿using System.Web.Mvc;
+﻿using HornsAndHoves.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web.Mvc;
 
 namespace HornsAndHoves.Controllers
 {
@@ -7,6 +13,35 @@ namespace HornsAndHoves.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+
+
+        public ActionResult List()
+        {
+            return Json(GetJsonAgency(), JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult GetSingle(int id)
+        {
+            return Json(GetJsonAgency(id), JsonRequestBehavior.AllowGet);
+        }
+
+        public List<Agency> GetJsonAgency(int id = 0)
+        {
+            List<Agency> agencies = new List<Agency>();
+
+            using (StreamReader r = new StreamReader("C:/tests/ffoms/HornsAndHoves/HornsAndHoves/agencies.json"))
+            {
+                string json = r.ReadToEnd();
+                agencies = JsonConvert.DeserializeObject<List<Agency>>(json);
+            }
+
+            if (id > 0)
+            {
+                return agencies.Where(x => x.Id == id).ToList();
+            }
+
+            return agencies;
         }
     }
 }
--- HornsAndHoves/HornsAndHoves/app.js
+++ HornsAndHoves/HornsAndHoves/app.js
@@ -1,24 +1,128 @@
 Ext.Loader.setConfig({ enabled: true });
+
+Ext.define('Agency', {
+    extend: 'Ext.data.Model',
+
+    idProperty: 'Id',
+
+    fields: [{
+        name: 'Id',
+        type: 'int'
+    },
+    { name: 'Name', type: 'string' },
+    { name: 'Surname', type: 'string' },
+    { name: 'FirstName', type: 'string' },
+    { name: 'Partonymic', type: 'string' },
+    { name: 'City', type: 'string' },
+    { name: 'Inn', type: 'string' },
+    { name: 'Kpp', type: 'string' },
+    { name: 'Ogrn', type: 'string' },
+    { name: 'Bik', type: 'string' },
+    { name: 'EmployeeCount', type: 'int' }
+    ]
+});
+
+let store = Ext.create('Ext.data.Store', {
+    model: 'Agency',
+    autoLoad: true,
+    proxy: {
+        type: 'ajax',
+        url: 'http://localhost:8000/Home/List',
+        reader: {
+            type: 'json',
+            root: 'Agency'
+        }
+    }
+});
+
+let grid = Ext.create('Ext.grid.Panel', {
+    title: 'Список организаций',
+    height: 500,
+    width: 1000,
+    store: store,
+    columns: [
+        { header: 'Название организации', dataIndex: 'Name' },
+        { header: 'ФИО учредителя', dataIndex: 'Surname', xtype: 'templatecolumn', tpl: '{Surname}.{FirstName}.{Partonymic}' },
+        { header: 'Город', dataIndex: 'City' },
+        { header: 'ИНН', dataIndex: 'Inn' },
+        { header: 'КПП', dataIndex: 'Kpp' },
+        { header: 'ОГРН', dataIndex: 'Ogrn' },
+        { header: 'БИК', dataIndex: 'Bik' },
+        { header: 'Количество сотрудников', dataIndex: 'EmployeeCount' },
+        {
+            xtype: 'actioncolumn', width: 40, items: [{
+                icon: 'editing.png',
+                handler: function (grid, rowIndex, colIndex) {
+                    //console.log(grid, rowIndex, colIndex);
+                    let selectionModel = grid.getSelectionModel(), record;
+                    selectionModel.select(rowIndex);
+                    record = selectionModel.getSelection()[0];
+                    //console.log(record.get('Id'));
+                    editAgency(record.get('Id'));
+                }
+            }]
+        }
+    ],
+    region: 'south',
+    //listeners: {
+    //    click: function () {
+    //        console.log(click);
+    //    }
+    //}
+    //margin: '37 0 0 0'
+});
+
+function editAgency(id) {
+    let singleAgencyStore = Ext.create('Ext.data.Store', {
+        model: 'Agency',
+        autoLoad: true,
+        proxy: {
+            type: 'ajax',
+            url: `http://localhost:8000/Home/GetSingle?id=${id}`,
+            reader: {
+                type: 'json',
+                root: 'Agency'
+            }
+        }
+    });
+
+
+
+    Ext.create('Ext.window.Window', {
+        title: singleAgencyStore.first().Surname,
+        height: 500,
+        width: 500,
+        layout: 'fit',
+        items: {
+
+        }
+    }).show();
+}
+
 Ext.application({
     name: 'HornsAndHoves',
     appFolder: 'app',
     controllers: [],
     requires: [
     ],
-    
+
     launch: function () {
         Ext.create('Ext.container.Viewport', {
             layout: 'border',
-            items: [{
-                region: 'center',
-                xtype: 'tabpanel',
-                activeTab: 0,
-                items: [
-                    {
-                        title: 'Организации'
-                    }
-                ]
-            }]
+            items: [
+                {
+                    region: 'center',
+                    xtype: 'tabpanel',
+                    activeTab: 0,
+                    items: [
+                        {
+                            title: 'Организации'
+                        },
+                    ]
+                }, grid
+            ]
         })
     }
-})
+});
+
+
